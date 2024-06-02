01b20371ef3ef12fe2f451aa818d49cc0055b62b
Lenovo <Lenovo@DESKTOP-O5UM8HU>
2024-06-02 16:52:47 +0630

    Add project files.

---
+++ Hello_World_Git/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Hello_World_Git
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
